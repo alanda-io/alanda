9e6409e27ca7fe95dd5101c8b150ae5032718260
Stephan Zavrel <stephan.zavrel@bpmasters.at>
2019-08-14 14:45:41 +0200

    moved persistence config to separate maven module

--- backend/alanda-development/src/main/resources/db-migration/alanda/migrate/releases/2019.00/02_alanda_demo.sql
+++ backend/alanda-development/src/main/resources/db-migration/alanda/migrate/releases/2019.00/02_alanda_demo.sql
@@ -1,10 +1,16 @@
 --liquibase formatted sql
 
--- --changeset fsa:alanda-demo-projecttype
+--changeset fsa:alanda-demo-projecttype
 Insert into PMC_PROJECTTYPE
  (GUID,VERSION,IDNAME,NAME,ALLOWEDTAGS,READRIGHTS,WRITERIGHTS,DELETERIGHTS,ALLOWEDPROCESSES,STARTPROCESS,OBJECTTYPE,ROLES,ALLOWED_SUBTYPES,ADDITIONAL_PROPERTIES,CONFIGURATION,LISTENERS,CREATERIGHTS,DETAILS_TEMPLATE,PROPERTIES_TEMPLATE,CREATION_PROPERTIES_TEMPLATE) values
  (1,1,'VACATION','Vacation Request','Tag1, Tag2, Tag3, Tag4','admin','admin','admin',null,'vacation-request','kk',null,null,null,null,'pmc-authorization','admin',null,'VACATION',null);
 
+
+--changeset yk:2019-06-camunda-auth runOnChange:false
+Insert into PMC_PERMISSION (GUID,VERSION,KEY) values (1,0,'project:create:VACATION');
+Insert into PMC_PERMISSION (GUID,VERSION,KEY) values (2,0,'project:read:VACATION');
+Insert into PMC_USER_PERMISSION (REF_USER,REF_PERMISSION) values (1,1);
+Insert into PMC_USER_PERMISSION (REF_USER,REF_PERMISSION) values (1,2);
 
 --changeset fsa:alanda-demo-users-and-groups
 insert into pmc_group
