1da166cf9792bfb4beef2810f21423135c496d87
Stephan Zavrel <stephan.zavrel@bpmasters.at>
2019-08-14 18:15:58 +0200

    added som efrontend exports to public-api

--- frontend/alanda-libraries/projects/alanda-common/src/public_api.ts
+++ frontend/alanda-libraries/projects/alanda-common/src/public_api.ts
@@ -14,6 +14,7 @@ export * from './lib/services/authorization.service';
 export * from './lib/services/exceptionHandling.service';
 export * from './lib/services/forms.service';
 export * from './lib/services/projectproperties.service';
+export * from './lib/services/projectdetails.service';
 
 export * from './lib/auth/basic-auth.interceptor';
 export * from './lib/auth/error.interceptor';
@@ -22,7 +23,10 @@ export * from './lib/models/PmcUser';
 export * from './lib/models/AppSettings';
 export * from './lib/models/RefObjectExtended';
 export * from './lib/models/RefObject';
+export * from './lib/models/project.model';
+export * from './lib/models/projectType.model';
 export * from './lib/alanda-common.module';
+
 
 export * from './lib/components/project-monitor/project-monitor.component';
 export * from './lib/components/tasklist/tasklist.component';
@@ -38,4 +42,6 @@ export * from './lib/components/attachments/attachments-list/attachments-list.co
 export * from './lib/components/attachments/attachments-tree/attachments-tree.component';
 export * from './lib/components/project-header/project-header.component';
 export * from './lib/components/create-project/create-project.component';
+export * from './lib/components/attachments/attachments.component';
+export * from './lib/components/forms/tasks/forms-controller/baseForm.component';
 
