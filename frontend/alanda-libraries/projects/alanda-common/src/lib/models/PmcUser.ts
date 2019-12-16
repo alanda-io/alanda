5df8e7d8961103adf0963728372c9b53a47abc8a
yannick-alanda <yannick.koitzsch@bpmasters.at>
2019-12-16 14:52:51 +0100

    add runAs-property to pmcUser

--- frontend/alanda-libraries/projects/alanda-common/src/lib/models/PmcUser.ts
+++ frontend/alanda-libraries/projects/alanda-common/src/lib/models/PmcUser.ts
@@ -1,20 +1,19 @@
-import { PmcPermission } from "./PmcPermission";
-
 export class PmcUser {
-  guid: number;
-  externalGuid: number;
-  firstName: string;
-  surname: string;
-  source: string;
-  company: string;
-  displayName: string;
-  loginName: string;
-  email: string;
-  mobile: string;
-  groups: any[];
-  roles: any[];
-  sso: boolean;
-  stringPermissions?: string[];
-  locked: boolean;
-  pmcDepartment: number;
+  public guid?: number;
+  public externalGuid?: number;
+  public firstName?: string;
+  public surname?: string;
+  public source?: string;
+  public company?: string;
+  public displayName?: string;
+  public loginName?: string;
+  public email?: string;
+  public mobile?: string;
+  public groups?: string[];
+  public roles?: string[];
+  public sso?: boolean;
+  public stringPermissions?: string[];
+  public locked?: boolean;
+  public pmcDepartment?: number;
+  public runAs?: string;
 }
