99cba56fcd7666b95cd266a0396374de6d514a4f
szavrel <stephan.zavrel2@drei.com>
2020-04-21 21:44:59 +0200

    refactor(ang-146): added npmrc, proxy conf fixes

--- frontend/alanda-libraries/src/app/app.settings.ts
+++ frontend/alanda-libraries/src/app/app.settings.ts
@@ -1,6 +1,6 @@
 import { AppSettings } from 'projects/alanda-common/src/lib/models/appSettings';
 
 export const ALANDA_CONFIG: AppSettings = {
-  API_ENDPOINT: '/drei-bpm-rest/app'
+  API_ENDPOINT: '/alanda-rest/app'
   /* WEBSOCKET_ENDPOINT : 'ws://localhost:8080/pmc-rest/websockets', */
 }
