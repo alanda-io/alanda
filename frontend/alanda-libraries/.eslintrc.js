20bb33147ca6e48b6b00d9c27b305da0066d53d2
Matthias Gallee <gallee.matthias@gmail.com>
2020-05-22 12:41:57 +0200

    Added eslint "standard with typescript" rules

--- frontend/alanda-libraries/.eslintrc.js
+++ frontend/alanda-libraries/.eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
-  extends: ['plugin:@angular-eslint/recommended'],
+  extends: ['standard-with-typescript', 'plugin:@angular-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: [
+      'tsconfig.app.json',
+      'tsconfig.spec.json',
+      'e2e/tsconfig.json',
+      'projects/alanda-common/tsconfig.lib.json',
+      'projects/alanda-common/tsconfig.spec.json'
+    ]
+  },
   rules: {
+    '@typescript-eslint/semi': [
+      'error',
+      'always',
+      { "omitLastInOneLineBlock": true }
+    ],
     // ORIGINAL tslint.json -> "component-selector": [true, "element", "app", "kebab-case"],
     '@angular-eslint/component-selector': [
       'warn',
