64ddf0767739cab43b446ee52a478c795749efe2
Matthias Gallee <gallee.matthias@gmail.com>
2020-06-04 15:39:39 +0200

    Created shareable eslint config for alanda

---
+++ frontend/alanda-libraries/projects/alanda-eslint-config-standard/index.js
@@ -0,0 +1,82 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'standard',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: [
+      'error',
+      'always',
+      { omitLastInOneLineBlock: true }
+    ]
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: [
+          'tsconfig.json',
+          'tsconfig.app.json',
+          'tsconfig.spec.json',
+          'e2e/tsconfig.json',
+          'projects/alanda-common/tsconfig.lib.json',
+          'projects/alanda-common/tsconfig.spec.json'
+        ]
+      },
+      plugins: [
+        '@typescript-eslint'
+      ],
+      extends: [
+        'standard-with-typescript',
+        'plugin:@angular-eslint/recommended'
+      ],
+      rules: {
+        '@typescript-eslint/semi': [
+          'error',
+          'always',
+          { omitLastInOneLineBlock: true }
+        ],
+        '@typescript-eslint/member-delimiter-style': [
+          'error',
+          {
+            multiline: {
+              delimiter: 'semi',
+              requireLast: true
+            },
+            singleline: {
+              delimiter: 'semi',
+              requireLast: false
+            }
+          }
+        ],
+        '@typescript-eslint/space-before-function-paren': [
+          'error',
+          'never'
+        ],
+        '@angular-eslint/component-selector': [
+          'warn',
+          { type: 'element', prefix: 'alanda', style: 'kebab-case' }
+        ],
+        'max-len': [
+          'off'
+        ],
+        'dot-notation': [
+          'off'
+        ]
+      },
+      overrides: [
+        {
+          files: ['*.component.ts'],
+          plugins: ['@angular-eslint/template'],
+          processor: '@angular-eslint/template/extract-inline-html'
+        }
+      ]
+    }
+  ]
+};
