de28d532ba997edc2757d93f05f6df777010e928
Matthias Gallee <gallee.matthias@gmail.com>
2020-06-29 14:22:24 +0200

    Fixed change directory fallback

--- start-dev.sh
+++ start-dev.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo -e '\e[1m > Stating docker images \n'
-cd ./backend/alanda-development/src/test/resources/
+cd ./backend/alanda-development/src/test/resources/ || { echo -e '\e[31mPath does not exist!'; exit 1; }
 docker-compose up -d
-cd ./docker-oracle/
+cd ./docker-oracle/ || { echo -e '\e[31mPath does not exist!'; exit 1; }
 docker-compose up -d
 cd ../../../../../../
 
 echo -e '\n---------------------------------------------\n'
 
 echo -e '\e[1m > Stating frontend for development'
-cd ./frontend/alanda-libraries
+cd ./frontend/alanda-libraries || { echo -e '\e[31mPath does not exist!'; exit 1; }
 npm run start
