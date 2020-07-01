c82ee0d3c53f067f9577e3897ed9022d6c9fbc11
Matthias Gallee <gallee.matthias@gmail.com>
2020-07-01 10:12:38 +0200

    Fixed path in start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo -e '\e[1m > Stating docker images \n'
-cd ./backend/alanda-development/src/test/resources/
+echo -e '\e[1m>> Stating docker images\e[0m \n'
+cd ./backend/alanda-development/src/test/resources/ || { echo -e '\e[31mPath does not exist!\e[0m'; exit 1; }
 docker-compose up -d
-cd ./docker-oracle/
+cd ./docker-oracle/ || { echo -e '\e[31mPath does not exist!\e[0m'; exit 1; }
 docker-compose up -d
 cd ../../../../../../
 
 echo -e '\n---------------------------------------------\n'
 
-echo -e '\e[1m > Stating frontend for development'
-cd ./frontend/alanda-libraries
+echo -e '\e[1m>> Stating frontend for development\e[0m'
+cd ./frontend/alanda || { echo -e '\e[31mPath does not exist!\e[0m'; exit 1; }
 npm run start
