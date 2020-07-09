725c271258e6967c2e9ecc53ed27c9a97e574d79
stephan <stephan@jungidee.at>
2020-07-09 10:43:04 +0200

    fixed module imports

--- frontend/alanda/libs/common/src/lib/components/phase-tab/phase-tab.module.ts
+++ frontend/alanda/libs/common/src/lib/components/phase-tab/phase-tab.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlandaPhaseTabComponent } from './phase-tab.component';
+import { MenuModule } from 'primeng/menu';
 
 @NgModule({
+  imports: [CommonModule, MenuModule],
   declarations: [AlandaPhaseTabComponent],
-  imports: [CommonModule],
   exports: [AlandaPhaseTabComponent],
 })
 export class PhaseTabModule {}
