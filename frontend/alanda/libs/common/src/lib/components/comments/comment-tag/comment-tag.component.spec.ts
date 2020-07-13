dc301feb73ef37a049bf379d1ae70d4b290d45a2
Matthias Gallee <gallee.matthias@gmail.com>
2020-07-13 11:37:53 +0200

    test(comments): Created test for comment tag component

---
+++ frontend/alanda/libs/common/src/lib/components/comments/comment-tag/comment-tag.component.spec.ts
@@ -0,0 +1,35 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AlandaCommentTagComponent } from './comment-tag.component';
+import { ButtonModule } from 'primeng/button';
+import { TemplateModule } from '@rx-angular/template';
+
+describe('AlandaCommentTagComponent', () => {
+  let component: AlandaCommentTagComponent;
+  let fixture: ComponentFixture<AlandaCommentTagComponent>;
+
+  const testTag = {
+    name: 'test',
+    type: 'test',
+    status: true
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AlandaCommentTagComponent ],
+      imports: [ ButtonModule, TemplateModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AlandaCommentTagComponent);
+    component = fixture.componentInstance;
+    component.tag = testTag;
+    component.isActive = true;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
