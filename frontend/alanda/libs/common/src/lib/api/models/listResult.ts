9db145d899d2df638e057015f58e8ca4161b5c43
Matthias Gallee <gallee.matthias@gmail.com>
2020-08-17 12:35:47 +0200

    fix(app): Fixed AlandaListResult type

--- frontend/alanda/libs/common/src/lib/api/models/listResult.ts
+++ frontend/alanda/libs/common/src/lib/api/models/listResult.ts
@@ -1,10 +1,4 @@
 export interface AlandaListResult<T> {
   total: number;
-  results: AlandaResult<T>[];
-}
-
-interface AlandaResult<T> {
-  [key: string]: T | any;
-  lastSyncTime?: number;
-  refObject?: any;
+  results: any[];
 }
