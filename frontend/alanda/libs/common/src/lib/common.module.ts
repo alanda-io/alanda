ed1a1389038ed3b443208aa737a68f7d3885b561
Matthias Gallee <gallee.matthias@gmail.com>
2020-09-03 12:08:37 +0200

    Upgrade NX workspace

--- frontend/alanda/libs/common/src/lib/common.module.ts
+++ frontend/alanda/libs/common/src/lib/common.module.ts
@@ -237,7 +237,7 @@ import { AttachmentsModule } from './components/attachments/attachments.module';
   ],
 })
 export class AlandaCommonModule {
-  public static forRoot(config: AppSettings): ModuleWithProviders {
+  public static forRoot(config: AppSettings): ModuleWithProviders<AlandaCommonModule> {
     return {
       ngModule: AlandaCommonModule,
       providers: [
