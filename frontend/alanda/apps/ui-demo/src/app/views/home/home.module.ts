c3f3a9979aa66830d438832c1b503e1fb68d6164
Matthias Gallee <gallee.matthias@gmail.com>
2020-09-04 13:48:34 +0200

    fix(app): Fixed font size scaling

---
+++ frontend/alanda/apps/ui-demo/src/app/views/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { CardModule } from 'primeng/card';
+import { AlandaCommonModule } from '@alanda/common';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    AlandaCommonModule,
+    CardModule,
+    ButtonModule,
+  ]
+})
+export class HomeModule { }
--- frontend/alanda/apps/ui-demo/src/app/views/views.module.ts
+++ frontend/alanda/apps/ui-demo/src/app/views/views.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
 import { ProjectMonitorModule } from './project-monitor/project-monitor.module';
 import { TaskListModule } from './task-list/task-list.module';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [ProjectMonitorModule, TaskListModule],
+  imports: [HomeModule, ProjectMonitorModule, TaskListModule],
   providers: [],
 })
 export class ViewsModule {}
