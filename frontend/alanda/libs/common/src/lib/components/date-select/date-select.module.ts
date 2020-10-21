823bef8c85ffbcf2d0fde39dff643ec153e9453e
Matthias Gallee <gallee.matthias@gmail.com>
2020-10-21 16:21:57 +0200

    fix(forms): Improved form layouts

--- frontend/alanda/libs/common/src/lib/components/date-select/date-select.module.ts
+++ frontend/alanda/libs/common/src/lib/components/date-select/date-select.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AlandaDateSelectComponent } from './date-select.component';
 import { CalendarModule } from 'primeng/calendar';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LabelModule } from '../label/label.module';
 
 @NgModule({
   declarations: [AlandaDateSelectComponent],
-  imports: [CommonModule, CalendarModule, ReactiveFormsModule],
+  imports: [CommonModule, CalendarModule, ReactiveFormsModule, LabelModule],
   exports: [AlandaDateSelectComponent],
 })
 export class DateSelectModule {}
