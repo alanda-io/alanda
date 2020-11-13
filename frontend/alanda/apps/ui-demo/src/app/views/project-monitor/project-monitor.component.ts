fa11cd994112d33a9c683a84311c7894e81d2e36
Matthias Gallee <gallee.matthias@gmail.com>
2020-11-13 12:34:12 +0100

    test(project-monitor): More layout test data

--- frontend/alanda/apps/ui-demo/src/app/views/project-monitor/project-monitor.component.ts
+++ frontend/alanda/apps/ui-demo/src/app/views/project-monitor/project-monitor.component.ts
@@ -15,7 +15,6 @@ export class AlandaProjectMonitorComponent {
         displayName: 'Project ID',
         name: 'Project ID',
         field: 'project.projectId',
-        width: 200,
       },
       { displayName: 'Title', name: 'Title', field: 'project.title' },
       {
@@ -45,7 +44,7 @@ export class AlandaProjectMonitorComponent {
         displayName: 'Prio',
         name: 'Prio',
         field: 'project.priority',
-        width: '40',
+        width: 75,
         template:
           "{'priority': true, 'priority-high': project.priority == 0, 'priority-medium': project.priority == 1, 'priority-low': project.priority == 2}",
       },
@@ -55,6 +54,34 @@ export class AlandaProjectMonitorComponent {
         name: 'Status',
         field: 'project.status',
       },
+    ];
+
+    const columnDefSimple = [
+      {
+        displayName: 'Project ID',
+        name: 'Project ID',
+        field: 'project.projectId',
+      },
+      { displayName: 'Title', name: 'Title', field: 'project.title' },
+      {
+        displayName: 'Project Type',
+        name: 'Project Type',
+        field: 'project.pmcProjectType.name',
+      },
+      {
+        displayName: 'Due Date',
+        name: 'Due Date',
+        field: 'project.dueDate',
+        type: TableColumnType.DATE,
+      },
+      {
+        displayName: 'Prio',
+        name: 'Prio',
+        field: 'project.priority',
+        width: 75,
+        template:
+          "{'priority': true, 'priority-high': project.priority == 0, 'priority-medium': project.priority == 1, 'priority-low': project.priority == 2}",
+      }
     ];
 
     this.layouts = [
@@ -73,13 +100,13 @@ export class AlandaProjectMonitorComponent {
       {
         name: 'foo',
         displayName: 'Foo',
-        columnDefs: columnDefAll,
+        columnDefs: columnDefSimple,
         filterOptions: {},
       },
       {
         name: 'bar',
         displayName: 'Bar',
-        columnDefs: columnDefAll,
+        columnDefs: columnDefSimple,
         filterOptions: {},
       },
     ];
