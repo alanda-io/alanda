69076a9e4cbcfaaca58b2136c8fb4aad605d71b4
Matthias Gallee <gallee.matthias@gmail.com>
2020-11-18 15:36:47 +0100

    fix(priority): Added index and improved error handling

--- frontend/alanda/libs/common/src/lib/components/project-table/project-table.component.ts
+++ frontend/alanda/libs/common/src/lib/components/project-table/project-table.component.ts
@@ -114,7 +114,8 @@ export class AlandaProjectTableComponent implements OnInit {
             summary: 'Load Tasks',
             detail: err.message,
           });
-          return of({ serverOptions, loading: false });
+          const projectsData = this.state.get().projectsData;
+          return of({ serverOptions, projectsData, loading: false });
         }),
       ),
     ),
