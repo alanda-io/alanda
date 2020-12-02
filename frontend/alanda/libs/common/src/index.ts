e0f01b06cd49c5be5b473f1484388ff31cf2c2dc
Matthias Gallee <gallee.matthias@gmail.com>
2020-12-02 12:24:39 +0100

    test hook

--- frontend/alanda/libs/common/src/index.ts
+++ frontend/alanda/libs/common/src/index.ts
@@ -7,13 +7,6 @@ export { APP_CONFIG, AppSettings } from './lib/models/appSettings';
 export { AlandaTaskFormPanel } from './lib/models/alandaTaskFormPanel';
 export * from './lib/permissions';
 
-
-
-
-
-
-
-
 /**
  * Api Exports
  */
