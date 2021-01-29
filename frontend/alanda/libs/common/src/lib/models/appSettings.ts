7b554125385f31bb1f2f1fde33e2544fafc3a6f6
Christoph Hagelkruys <christoph.hagelkruys@alanda.io>
2021-01-29 15:13:52 +0100

    fix(make constant injectable)

--- frontend/alanda/libs/common/src/lib/models/appSettings.ts
+++ frontend/alanda/libs/common/src/lib/models/appSettings.ts
@@ -4,6 +4,13 @@ import { SelectItem } from 'primeng/api';
 
 @Injectable()
 export class AppSettings {
+  public static ALANDA_PRIORITIES: SelectItem[] = [
+    { value: -1, label: '-1 - Emergency plus' },
+    { value: 0, label: '0 - Emergency' },
+    { value: 1, label: '1 - Urgent' },
+    { value: 2, label: '2 - Normal' },
+  ];
+
   API_ENDPOINT: string;
   AVATAR_EXT: string;
   AVATAR_BASE_PATH: string;
@@ -13,12 +20,5 @@ export class AppSettings {
   CLOSE_AFTER_COMPLETE: boolean;
   PRIORITIES: SelectItem[];
 }
-
-export const ALANDA_PRIORITIES: SelectItem[] = [
-  { value: -1, label: '-1 - Emergency plus' },
-  { value: 0, label: '0 - Emergency' },
-  { value: 1, label: '1 - Urgent' },
-  { value: 2, label: '2 - Normal' },
-];
 
 export const APP_CONFIG = new InjectionToken<AppSettings>('AppConfig');
